4efe9e43566be9980ed780f90dad6c52f85701d6
Stephanie <stephogam@gmail.com>
2021-03-01 21:01:01 +0000

    Attempted to create word count function. Errors still persist. Co-authored-by: Tina Haiser <tinahaiser@gmail.com>

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,18 @@
 # put your code here.
+
+
+def word_count(file):
+  word_count_data = open(file)
+
+  for line in word_count_data:
+    file_data = line.split(" ")
+
+  word_count_dict = {}
+
+  for word in file_data:
+    word_count_dict[word] = +1
+  print(f"{word_count_dict.keys()}, {word_count_dict.values()}")
+  
+  word_count_data.close()
+  
+word_count("test.txt")
